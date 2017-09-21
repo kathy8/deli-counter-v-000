cac6f2845e7f3a85d55aa8a23bac089bd2e54efb
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-21 18:13:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
-katz_deli = []
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length > 1
-    count []
+    count = []
     katz_deli.each_with_index do |name, index|
       count << (" #{index+1}. #{name}")
     end
