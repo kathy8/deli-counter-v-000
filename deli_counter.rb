422023e8c04755104118d048d46fa04cd751f2bf
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-21 18:23:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,6 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, new_customer)
-  katz_deli << name
-  number = katz_deli.length
   puts "Welcome, #{new_customer}. You are number #{katz_deli.length +1}in line."
 katz_deli << new_customer
 end
