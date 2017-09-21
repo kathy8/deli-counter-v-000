092aaaaf068dc5bc5e3396ddd8c8c305717c756b
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2017-09-21 18:06:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,13 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else katz_deli.length > 1
-    
-    message = "The line is currently"
+    count []
+    katz_deli.each_with_index do |name, index|
+      count << (" #{index+1}. #{name}")
+    end
+    puts "The line is currently:" +count.join
+  end
+end
+
+
+  
